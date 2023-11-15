bbb56fc1f545887afda0ea76e06eba82ce46e956
jeiderws <jeiderm20@gmail.com>
2023-11-15 07:32:07 -0500

    evidencia cronometro y temporizadoneb

--- app/src/main/java/com/example/reloj/MainActivity2.kt
+++ app/src/main/java/com/example/reloj/MainActivity2.kt
@@ -17,72 +17,72 @@ class MainActivity2 : AppCompatActivity() {
         binding = ActivityMain2Binding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.hoursPicker.minValue = 0
-        binding.hoursPicker.maxValue = 23
+        binding.horaPick.minValue = 0
+        binding.horaPick.maxValue = 23
 
-        binding.minutesPicker.minValue = 0
-        binding.minutesPicker.maxValue = 59
+        binding.minutosPick.minValue = 0
+        binding.minutosPick.maxValue = 59
 
-        binding.secondsPicker.minValue = 0
-        binding.secondsPicker.maxValue = 59
+        binding.segundosPick.minValue = 0
+        binding.segundosPick.maxValue = 59
         binding.startButton.setOnClickListener {
-            startTimer()
+            iniciartemp()
         }
         binding.stopButton.setOnClickListener {
-            stopTimer()
+            parartemp()
         }
         binding.startButtoncro.setOnClickListener {
-            startChronometer()
+            iniciarcrono()
         }
         binding.stopButtoncro.setOnClickListener {
-            stopChronometer()
+            paracrono()
         }
     }
 
-    private fun startTimer() {
-        val totalMilliseconds =
-            (binding.hoursPicker.value * 3600000L) +
-                    (binding.minutesPicker.value * 60000L) +
-                    (binding.secondsPicker.value * 1000L)
+    private fun iniciartemp() {
+        val totalmilseg =
+            (binding.horaPick.value * 3600000L) +
+                    (binding.minutosPick.value * 60000L) +
+                    (binding.segundosPick.value * 1000L)
 
         binding.startButton.isEnabled = false
         binding.stopButton.isEnabled = true
 
-        timer = object : CountDownTimer(totalMilliseconds, 1000) {
-            override fun onTick(millisUntilFinished: Long) {
-                val hours = millisUntilFinished / 3600000
-                val minutes = (millisUntilFinished % 3600000) / 60000
-                val seconds = (millisUntilFinished % 60000) / 1000
+        timer = object : CountDownTimer(totalmilseg, 1000) {
+            override fun onTick(finalmiliseg: Long) {
+                val hours = finalmiliseg / 3600000
+                val minutes = (finalmiliseg % 3600000) / 60000
+                val seconds = (finalmiliseg % 60000) / 1000
 
-                binding.hoursPicker.value = hours.toInt()
-                binding.minutesPicker.value = minutes.toInt()
-                binding.secondsPicker.value = seconds.toInt()
+                binding.horaPick.value = hours.toInt()
+                binding.minutosPick.value = minutes.toInt()
+                binding.segundosPick.value = seconds.toInt()
             }
 
             override fun onFinish() {
-                stopTimer()
+                parartemp()
                 Toast.makeText(this@MainActivity2, "tiempo finalizao juan soy un cracken:(", Toast.LENGTH_SHORT).show()
             }
         }.start()
     }
 
-    private fun stopTimer() {
+    private fun parartemp() {
         timer?.cancel()
         binding.startButton.isEnabled = true
         binding.stopButton.isEnabled = false
     }
 
-    private fun startChronometer() {
+    private fun iniciarcrono() {
         if (!isRunning) {
-            binding.chronometer.base = SystemClock.elapsedRealtime() // este vainito Resetea el cronómetro
-            binding.chronometer.start()
+            binding.cronometro .base = SystemClock.elapsedRealtime() // este vainito Resetea el cronómetro
+            binding.cronometro .start()
             isRunning = true
         }
     }
 
-    private fun stopChronometer() {
+    private fun paracrono() {
         if (isRunning) {
-            binding.chronometer.stop()
+            binding.cronometro .stop()
             isRunning = false
         }
     }
